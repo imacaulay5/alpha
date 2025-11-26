76d86bc223b8fcbbd4814e5170930b9bfbf54c91
iver macaulay <macauive@icloud.com>
2025-11-25 18:50:43 -0600

    Initial Commit

---
+++ alpha/alphaApp.swift
@@ -0,0 +1,17 @@
+//
+//  alphaApp.swift
+//  alpha
+//
+//  Created by Iver Macaulay on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct alphaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
